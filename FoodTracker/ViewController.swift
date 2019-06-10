9f1723ec791eaf3fbc327c46bebda8f83ec761ae
Stephane Gagnon <stephane.gagnon00@gmail.com>
2019-06-10 14:59:37 -0400

    Basic UI implemented, auto layout implemented. No code yet

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by TempAcc on 2019-06-10.
+//  Copyright © 2019 StephG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
